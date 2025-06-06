4f7da705e5086f7e234420238823bb2407ad60a5
TamoghnaCG <tamoghna.roy@capgemini.com>
2025-06-06 15:25:09 +0530

    Add map and pointer on map using Global state

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 import styles from "./Map.module.css";
-import { useNavigate, useSearchParams } from 'react-router-dom';
-
-
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useCity } from "../contexts/CityContext";
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [position, setPosition] = useState([51.505, -0.09]);
+  const { cities } = useCity();
   const navigate = useNavigate();
-let lat =  searchParams.get("lat")
-let lng =  searchParams.get("lng")
+  let lat = searchParams.get("lat");
+  let lng = searchParams.get("lng");
   return (
-    <div className={styles.mapContainer} onClick={()=>{navigate("form")}}>
-      <div>
-
-      Map
-      position : {lat}, {lng}
-      </div>
-<button onClick={()=>{setSearchParams({lat:786, lng:878})}}>Change position</button>
-
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={position}
+        zoom={5}
+        scrollWheelZoom={true}
+        className={styles.mapContainer}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+            <Popup>
+             {city.notes|| <span>No notes available for this city.</span>}
+              <br />
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
-  )
+  );
 }
 
-export default Map
+export default Map;
